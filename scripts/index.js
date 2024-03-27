96aa9ff700f145c97a184729d05e18568156a595
Olga Boris <olya_boris97@mail.ru>
2024-03-27 17:07:56 +0300

    update

--- scripts/index.js
+++ scripts/index.js
@@ -127,3 +127,16 @@ $(".team_items-slider").slick({
         '  </svg></div>',
 });
 
+function openInfo(evt, info) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(info).style.display = "block";
+    evt.currentTarget.className += " active";
+}
